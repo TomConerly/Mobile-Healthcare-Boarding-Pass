11f055c2cf19d8014939afbba4bd0b1599ebb716
Tom Conerly <tconerly@fb.com>
2016-11-05 10:40:58 -0400

    initial commit

---
+++ app/src/main/java/hackathon/com/mobile_healthcare_boarding_pass/MainActivity.java
@@ -0,0 +1,13 @@
+package hackathon.com.mobile_healthcare_boarding_pass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
