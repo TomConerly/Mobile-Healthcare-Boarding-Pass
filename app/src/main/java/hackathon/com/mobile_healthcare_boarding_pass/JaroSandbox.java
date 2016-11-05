1225e00f71cc0d6bda11a7b6ee6f0316ae26474d
Jaroslaw Blasiok <jaro3000@gmail.com>
2016-11-05 14:34:09 -0400

    NotificationService

--- app/src/main/java/hackathon/com/mobile_healthcare_boarding_pass/JaroSandbox.java
+++ app/src/main/java/hackathon/com/mobile_healthcare_boarding_pass/JaroSandbox.java
@@ -5,18 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class JaroSandbox extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jaro_sandbox);
 
     }
 
-    public void showNotification(View view) {
-
+    public void showNotification(View view)  {
+        String myToken = FirebaseInstanceId.getInstance().getToken();
+        Log.e("tag", myToken);
+        TextView tv = (TextView)findViewById(R.id.textView2);
+        if(tv != null) {
+            tv.setText(myToken);
+        }
     }
 
 }
---
+++ app/src/main/java/hackathon/com/mobile_healthcare_boarding_pass/NotificationService.java
@@ -0,0 +1,14 @@
+package hackathon.com.mobile_healthcare_boarding_pass;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class NotificationService extends FirebaseInstanceIdService {
+    public NotificationService() {
+    }
+
+}
