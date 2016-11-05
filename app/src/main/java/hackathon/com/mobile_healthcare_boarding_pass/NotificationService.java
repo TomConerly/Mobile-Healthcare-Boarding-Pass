306e3228bafa77b412c4529a4ef6af02cfa211be
Jaroslaw Blasiok <jaro3000@gmail.com>
2016-11-05 15:35:34 -0400

    Notification service

--- app/src/main/java/hackathon/com/mobile_healthcare_boarding_pass/NotificationService.java
+++ app/src/main/java/hackathon/com/mobile_healthcare_boarding_pass/NotificationService.java
@@ -1,14 +1,26 @@
 package hackathon.com.mobile_healthcare_boarding_pass;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class NotificationService extends FirebaseInstanceIdService {
-    public NotificationService() {
+public class NotificationService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.drawable.notification_icon);
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(0, builder.build());
     }
 
 }
